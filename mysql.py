818e6667198b542fa4b2a02abcf503520301f479
avazbek <eshmurodov.avazbek@mail.ru>
2022-02-18 15:56:25 +0500

    fix

--- mysql.py
+++ mysql.py
@@ -1,29 +1,12 @@
-import pymysql.cursors
-def getConnection():
-    # Вы можете изменить параметры соединения.
-    connection = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='TeleBot@2022@ok',
-        db='find',
-        charset='utf8',
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    return connection
-connection = getConnection()
-print ("Connect successful!")
-sql = "Select * from message "
-try :
-    cursor = connection.cursor()
-    # Выполнить sql и передать 1 параметр.
-    cursor.execute(sql, ( 10 ) )
-    print ("cursor.description: ", cursor.description)
-    print()
-    for row in cursor:
-        print (" ----------- ")
-        print("id: ", row['id'])
-        print ("message: ", row["message"])
-        print ("id_chat: ", row["id_chat"])
-finally:
-    # Закрыть соединение
-    connection.close()
+import MySQLdb
+
+mydb = MySQLdb.connect(
+    host='localhost',
+    user='root',
+    password='TeleBot@2022@ok',
+    db='find',
+)
+cur = mydb.cursor()
+command = cur.execute('SELECT * FROM message')
+results = cur.fetchall()
+print (results)
